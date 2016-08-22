e1afc7e67500cec228cf984108f2a7945ab5ff79
Tomas Kubica <tkubica12@gmail.com>
2016-08-22 05:40:58 +0200

    PowerShell script to deploy in Azure

---
+++ oneview-in-azure/README.md
@@ -0,0 +1,3 @@
+# OneView deployment to Azure
+
+This is PowerShell script to deploy OneView DCS appliance to Azure. Get you vhdx virtual hard disk file and rename it to OneView2.vhdx as script expects this names and will convert it to vhd used in Azure. Also resource group a storage account name is hardcoded, you might need to change this to fit your needs.
---
+++ oneview-in-azure/azure-deploy.ps1
@@ -0,0 +1,24 @@
+Convert-VHD -Path OneView2.vhdx -DestinationPath OneView2.vhd
+
+Login-AzureRmAccount
+New-AzureRmResourceGroup -Name OneView -Location westeurope
+New-AzureRmStorageAccount -ResourceGroupName OneView -Name oneview -Location westeurope -SkuName Standard_LRS -Kind "Storage"
+
+Add-AzureRmVhd -ResourceGroupName OneView -Destination "http://oneview.blob.core.windows.net/images/OneView2.vhd" -LocalFilePath OneView2.vhd
+
+$singleSubnet = New-AzureRmVirtualNetworkSubnetConfig -Name ovsubnet -AddressPrefix "10.20.1.0/24"
+$vnet = New-AzureRmVirtualNetwork -Name ovnet -ResourceGroupName OneView -Location westeurope -AddressPrefix "10.20.1.0/24" -Subnet $singleSubnet
+$ovip = New-AzureRmPublicIpAddress -Name ovip -ResourceGroupName OneView -Location westeurope -AllocationMethod Dynamic
+$ovnic = New-AzureRmNetworkInterface -Name ovnic -ResourceGroupName OneView -Location westeurope -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $ovip.Id
+
+$password = ConvertTo-SecureString "HPEnet123" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("oneview", $password);
+
+$storageAcc = Get-AzureRmStorageAccount -ResourceGroupName OneView -AccountName oneview
+$vmConfig = New-AzureRmVMConfig -VMName ovvm -VMSize Standard_D3_v2
+$vm = Set-AzureRmVMOperatingSystem -VM $vmConfig -Linux -ComputerName ovvm -Credential $cred
+$vm = Add-AzureRmVMNetworkInterface -VM $vm -Id $ovnic.Id
+$osDiskUri = '{0}vhds/ovvm-OneView2.vhd' -f $storageAcc.PrimaryEndpoints.Blob.ToString()
+$vm = Set-AzureRmVMOSDisk -VM $vm -Name ovvm-OneView2.vhd -VhdUri $osDiskUri -CreateOption fromImage -SourceImageUri http://oneview.blob.core.windows.net/images/OneView2.vhd -Linux
+
+New-AzureRmVM -ResourceGroupName OneView -Location westeurope -VM $vm
